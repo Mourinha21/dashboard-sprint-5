ec39f3f9fbce28332d271caddce099dd242ad3e8
Mourinha <rickgomes5844@gmail.com>
2025-12-03 21:13:40 -0300

    Criação dos botões ára criar os gráficos.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # dashboard-sprint-5
+
 Projeto de Dashboard para a Sprint 5 da Triple Ten
+
+Nesse projeto, eu peguei 4 DataFrames de 4 regiões do mundo, e nesses DataFrames possuem dados de empresas, número de funcionários, rank da forbes, receita, crescimento de receita e etc.
+
+No primeiro gráfico, confesso que não saiu muito legal, pois a minha ideia era fazer um gráfico de barra para representar a média de receita de cada país europeu, mas como o projeto precisava de um histograma, então usei esse primeiro.
+Esse primeiro gráfico ilustra a quantidade de receitas de acordo com o valor, e podemos ver um pico ali entre 50 bilhões à 99.9 bilhões, podemos também notar os valores de 300 bilhões para mais, que possuem apenas 2 empresas, mostrando que a disparidade dessas empresas para a média da receita.
+
+No segundo gráfico, fiz um gráfico de dispersão para ilustrar o valor do patrimônio de cada empresa para a sua posição no ranking, conseguimos notar que não necessariamente a empresa com maior patrimônio estará na frente, no lado direito isso é menos perceptível, tem alguns que possuem um patrimônio menor mas que estão na frente de outras empresas, aqui a variância não é tão brusca. Já no lado esquerdo do gráfico, que aproximamos mais do ranking n°1, a empresa na posição 55° tem um patrimônio absurdo em comparação com os outros colocados, mas ele não é o primeiro do gráfico, na posição n° 49 possui 1/4 do patrimônio da empresa n° 55, o que mostra que não podemos falar que o ranking é puramente pelo tamanho do patrimônio da empresa.
+
+No terceiro gráfico, tive a ideia de comparar o n° de funcionário com o lucro das empresas, como será que eles se relacionam? Bom, se formos pelo o gráfico, uma média boa seria de ter entre 10.000 e 20.000, é bastante funcionário, mais do que isso, parece não ser tão bom.
+
+E por último, esse gráfico compara a porcentagem de crescimento de várias indústrias, a grande maioria cresceu normalmente, os 2 que ficaram em primeiros, Financial e Technology, teve um aumento colossal em comparação ao resto. A minha hipótese é que esse resultado é diretamente relacionado as FinTechs que cada vez mais estão se fortalecendo no mercado devido as suas inovações e desburocratização. Já no outro lado do gráfico, os 2 últimos são relacionados a indústria agrícola e produção de alimentos, especulo que seja pela a concorrência de outros países que conseguem providenciar esses tipos de produtos por um preço mais em conta, nesse ano de 2025, o presidente dos EUA, implantou tarifas pesadas para a importação de diversos setores, com o objetivo de tentar fortalecer, forçando as redes de supermercado a comprarem produtos nacionais.
--- app.py
+++ app.py
@@ -1,3 +1,112 @@
 import streamlit as st
 import pandas as pd
-import plotly_express as plt
+import plotly_express as px
+
+
+# Armazenando os dataframes em variáveis
+
+dados_europa = pd.read_csv(r'C:\Users\madki\dashboard-sprint-5\Europe.csv')
+dados_india = pd.read_csv(r'C:\Users\madki\dashboard-sprint-5\India.csv')
+dados_filipinas = pd.read_csv(
+    r'C:\Users\madki\dashboard-sprint-5\Philippines.csv')
+dados_eua = pd.read_csv(r'C:\Users\madki\dashboard-sprint-5\United_States.csv')
+
+# Renomeando a coluna "Revenue growth" do dataframe dos dados dos EUA
+dados_eua = dados_eua.rename(columns={'Revenue growth': 'Revenue growth (%)'})
+
+# Alterando o tipo da coluna 'Profit(billions US$) de object para float'
+
+dados_india['Profit(billions US$)'][30] = -0.4
+dados_india['Profit(billions US$)'] = pd.to_numeric(
+    dados_india['Profit(billions US$)'])
+
+# Alterando os dados de um índice para conseguir converter a coluna no dataframe dos dados das Filipinas
+dados_filipinas['Profits(USD millions)'][24] = '12'
+
+# Alterando o tipo das colunas do dataframe das Filipinas
+
+dados_filipinas['Revenue(USD millions)'] = (
+    dados_filipinas['Revenue(USD millions)'].str.replace(",", ".").astype(float))
+dados_filipinas['Profits(USD millions)'] = (
+    dados_filipinas['Profits(USD millions)'].str.replace(",", ".").astype(float))
+dados_filipinas['Employees'] = (
+    dados_filipinas['Employees'].str.replace(",", "").astype(float))
+
+# Ajustando e depois convertendo as colunas que não eram do tipo certo
+dados_eua['Revenue (USD millions)'] = pd.to_numeric(
+    dados_eua['Revenue (USD millions)'].str.replace(",", ".").astype(float))
+dados_eua['Revenue growth (%)'] = pd.to_numeric(
+    dados_eua['Revenue growth (%)'].str.replace("%", "").astype(float))
+dados_eua['Employees'] = dados_eua['Employees'].str.replace(",", ".")
+dados_eua['Employees'] = dados_eua['Employees'].str.replace(".", "")
+dados_eua['Employees'] = pd.to_numeric(dados_eua['Employees'].astype(float))
+
+# Agrupando dados da Europa por país
+receita_pais_europeu = (
+    dados_europa
+    .groupby(['Headquarters'])[['Revenue(US$ billions)']]
+    .mean().reset_index()
+)
+
+receita_pais_europeu.sort_values(
+    by='Revenue(US$ billions)', axis=0, ascending=True, inplace=True)
+
+# Ordenando os dados do dataframe da Índia para a criação do gráfico
+dados_india.sort_values(by='Forbes 2000 rank', axis=0,
+                        ascending=False, inplace=True)
+
+# Atribuindo os dados ordenados do dataframe das Filipinas para a criação do gráfico
+dados_ordenados_filipinas = dados_filipinas.sort_values(
+    by='Employees', axis=0, ascending=True)
+
+# Atribuindo os dados dos EUA em uma nova variável para a criação do gráfico
+crescimento_industria_eua = (dados_eua.groupby(['Industry'])[
+                             ['Revenue growth (%)']].sum().reset_index())
+crescimento_industria_eua.sort_values(
+    by='Revenue growth (%)', ascending=True, inplace=True)
+
+hist_button_eu = st.button('Criar histograma')
+
+if hist_button_eu:
+    st.write(
+        'Criando um histograma para o conjunto de dados da receita de países europeus')
+
+    fig = px.histogram(dados_europa, x='Revenue(US$ billions)',
+                       title='Contagem de receitas dos países europeus', color_discrete_sequence=['indianred'])
+
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button_india = st.button('Criar gráfico de dispersão')
+
+if scatter_button_india:
+    st.write('Criando um gráfico de dispersão para o conjunto de dados da Índia')
+
+    fig = px.scatter(dados_india, x='Forbes 2000 rank',
+                     y='Assets(billions US$)',
+                     trendline='expanding',
+                     title='Patrimônio de empresas de acordo com o ranking da Forbes')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+line_button_filipinas = st.button('Criar gráfico de linha')
+
+if line_button_filipinas:
+    st.write('Criando um gráfico de linha para o conjunto de dados das Filipinas')
+
+    fig = px.scatter(dados_ordenados_filipinas, x='Employees',
+                     y='Profits(USD millions)',
+                     title='Lucro de empresas (por milhões de US$) pelo o número de funcionários (por mil)')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+area_button_eua = st.button('Criar gráfico de área')
+
+if area_button_eua:
+    st.write('Criando um gráfico de área para o conjunto de dados dos EUA')
+
+    fig = px.area(crescimento_industria_eua,
+                  x='Industry',
+                  y='Revenue growth (%)',
+                  title='Crescimento de receita das empresas estadunidenses por indústria')
+
+    st.plotly_chart(fig, use_container_width=True)
